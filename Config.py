9efffbd34c7810a671beead8a6b3c99e34d057af
ArtOfCode- <owen.jenkins2903@gmail.com>
2015-06-30 20:43:27 +0100

    add config

---
+++ Config.py
@@ -0,0 +1,18 @@
+class Config:
+    
+    General = {
+        "owner_name": "ArtOfCode", # Required
+        "owners": [ { "stackexchange.com": 121520 }, { "stackexchange.com": 146754 }, { "stackexchange.com": 138766 } ], # you can add multiple owners
+        "chatbot_name": "KarmaBot", # Required
+        "email": "opensource.se.chatbot@gmail.com", # Optional
+        "password": "chatbot.se.opensource" # Optional, and NOT RECOMMENDED
+		#"email": "webmaster.pmdg@gmail.com",
+		#"password": "0044o20w8882e0600n"
+    }
+    
+    Configurations = { # Optional, add configurations here
+        "site": "stackexchange.com",
+        "room": 25323,
+    }
+
+    
